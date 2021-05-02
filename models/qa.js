9703268051a1518666541df398bc1de21abef63f
Angelines Yaport-Garcia <angelinesyaport-garcia@Angeliness-MBP.fios-router.home>
2021-05-01 21:30:08 -0400

    Mom Blogs7

--- models/qa.js
+++ models/qa.js
@@ -1,31 +1,45 @@
 const qa = [
    {
-      question: 'question1',
-      answer: 'answer1',
+      question: 'How do you handle Sibling rivalry?',
+      answer1: `I make the hug or wrap them in a shirt together till they calm down, and then I explain to them why siblings should't fight.`,
+      answer: ``,
+      answer: ``,
    },
    {
-      question: 'question2',
-      answer: 'answer2',
+      question: 'Do you kiss your kids on the lips?',
+      answer1: `Some expers say it's ok to do so. However, they stress taking extra precautions as you can easily spread cold sores, strep, stomach viruses, bacterial infections, respiratory viruses, among other thins, since babies and young childre's immune systems aren't strong enough and still develping.(goodhousekeeping.com)`,
+      answer2: `Eww, no! I feel that if you allow your child to think it's ok to kiss you on the lip, they may think it's ok for some other person to do it as well, which could be very dangerous.`,
+      answer3: ``,
    },
    {
-      question: 'question3',
-      answer: 'answer3',
+      question: 'What is your response to picky eaters?',
+      answer1: `Experts say: parents shoudl avoid making separate meals fro children who are picky. The trick is how you, as a parent, react to it. Saying calm and avoiding threats/punishment is important to avoid negative experiences during meals(goodhousekeeping.com)`,
+      answer2: `I cook one meal. Unless I am certain that they abosolutely have tried it and REALLY don't like it, then I make them something else. If I know they are just being picky the may just sit there until they eat it, and in most cases they do.`,
+      answer3: ``,
    },
    {
       question: 'question4',
-      answer: 'answer4',
+      answer1: ``,
+      answer2: ``,
+      answer3: ``,
    },
    {
       question: 'question5',
-      answer: 'answer5',
+      answer1: ``,
+      answer2: ``,
+      answer3: ``,
    },
    {
       question: 'question6',
-      answer: 'answer6',
+      answer1: ``,
+      answer2: ``,
+      answer3: ``,
    },
    {
       question: 'question7',
-      answer: 'answer7',
+      answer1: ``,
+      answer2: ``,
+      answer3: ``,
    },
 ]
 module.exports = qa
