38a2855b515dad3dc6e126a2fb3929c3845f530f
Angelines Yaport-Garcia <angelinesyaport-garcia@Angeliness-MBP.fios-router.home>
2021-05-02 20:26:36 -0400

    Mom Blog

--- models/qa.js
+++ models/qa.js
@@ -1,45 +1,34 @@
 const qa = [
    {
       question: 'How do you handle Sibling rivalry?',
-      answer1: `I make the hug or wrap them in a shirt together till they calm down, and then I explain to them why siblings should't fight.`,
-      answer: ``,
-      answer: ``,
+      answer1: `I make them hug or wrap them in a shirt together till they calm down, and then I explain to them why siblings should't fight.`,
+      answer2: `Whatever they are fighting about, I take it away. If they can't agree on something together and/or share, then neither of them gets anything. Either they learn to share and agree on what they want to do together or nobody gets anthing. It seems to work because then one of them gives in and they begin a conversation among themselves and end up coming to a solution.`,
+      answer3: `Oh, I let them fight! Just kidding. I take away their favorite things for a certain period of time. When they ask for that favorite thing, I take advantage and begin a more indepth conversation about why it was take away in the first place and how siblings should treat each other. I remind them that they are sibligs and friends who should always be there for each other no matter what.`,
    },
    {
       question: 'Do you kiss your kids on the lips?',
       answer1: `Some expers say it's ok to do so. However, they stress taking extra precautions as you can easily spread cold sores, strep, stomach viruses, bacterial infections, respiratory viruses, among other thins, since babies and young childre's immune systems aren't strong enough and still develping.(goodhousekeeping.com)`,
       answer2: `Eww, no! I feel that if you allow your child to think it's ok to kiss you on the lip, they may think it's ok for some other person to do it as well, which could be very dangerous.`,
-      answer3: ``,
+      answer3: `No. I feel that is innapropriate and sends the wrong message and confuses the child`,
    },
    {
       question: 'What is your response to picky eaters?',
       answer1: `Experts say: parents shoudl avoid making separate meals fro children who are picky. The trick is how you, as a parent, react to it. Saying calm and avoiding threats/punishment is important to avoid negative experiences during meals(goodhousekeeping.com)`,
       answer2: `I cook one meal. Unless I am certain that they abosolutely have tried it and REALLY don't like it, then I make them something else. If I know they are just being picky the may just sit there until they eat it, and in most cases they do.`,
-      answer3: ``,
+      answer3: `I once thought it was the end of the world if my kid didn't eat and would make several different meals. Then one doctor told me not to do that and that when the child is hungry and realizes there's nothing else he/she will eat. I thought the doctor had lost his mind, but I decided to give it a try and it worked! I have not made more than one meal at a time since.`,
    },
    {
-      question: 'question4',
-      answer1: ``,
-      answer2: ``,
-      answer3: ``,
+      question: 'How do you single ladies date?',
+      answer1: `I don't! LOL. I find it extemely hard to date with my kids being so young and their dad being a little nuts. Plus I don't even have time even if I wanted to. Between work and motherhood, there's no time for dating.`,
+      answer2: `I've gone on a few dates here and there. But there's always that bug in the back of my mind, questioning if I'm being selfish for leaving my child with someone while I try to fullfill my own needs. And then there's that other bug, like a broken record in my head, saying: do you really wanna bring another man into your child's life? I've kind of given up hope for now. Maybe when they're older. `,
+      answer3: `It's definitely a chalenge. But I believe in 100% communication with my children and my significant other. Everyone has to be comfortable with everyone. My kids will ALWAYS come first, no matter what. And if they aren't comfortable with a man I date, I will listen to them, find out why, and if they have good reason, then so be it, the guy is dumped. But it all begins with communication. Telling the guy first hand about my children and that they are first in my life. Giving the guy some ground rules and what expected of him for being around my chidren if and when I allow him to be. `,
    },
    {
-      question: 'question5',
-      answer1: ``,
-      answer2: ``,
-      answer3: ``,
+      question: 'What do you do when your child throws a tantrum in public?',
+      answer1: `I do not tolerate that. My child will get a spank in the but and taken out to the car or the parkinglot, either till he/she calms down or we just go home and deal with it at home.`,
+      answer2: `I talk to my kids first and explain why they shouldn't behave that way. If they continue, we leave empty handed.`,
+      answer3: `I can't always afford to leave the store or take time out in the parking lot. When I have the time, I do. But I'm a busy mom, always on the run. I will take my kid aside, spank his but and look firmly in his eyes, and with a stern voice tell him/her to quiet down or else. If that doesn't work, then I do leave. But for me it usually works because I have never liked or tolerated that, so since they were very young I made sure to disciplin that and put that on check immediately.`,
    },
-   {
-      question: 'question6',
-      answer1: ``,
-      answer2: ``,
-      answer3: ``,
-   },
-   {
-      question: 'question7',
-      answer1: ``,
-      answer2: ``,
-      answer3: ``,
-   },
+
 ]
 module.exports = qa
