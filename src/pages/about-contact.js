9b28a90d36230904b72283704a7021f12cbfe031
AngelinesYG <79059491+AngelinesYG@users.noreply.github.com>
2021-08-28 22:51:09 -0400

    pages added

---
+++ src/pages/about-contact.js
@@ -0,0 +1,25 @@
+import React from 'react' 
+
+const AboutContact = () =>{
+    return(
+        <div>
+           <div className="About-container">
+              <h1 id="AboutMe">About the Developer</h1>
+            
+            
+           
+
+             <div id="text">
+             <h4>I am a mother to four amazing boys, who is filled with a passion for writing and creativity. My strength lies in my ability to problem solve with a keen attention to detail. These are qualities that support my work as a Developer.
+
+              My resilient and steadfast attitude makes me a strong woman who will not give up easily. Aside from my love for developing useful apps, I enjoy spending time with my children, listening to the calming sound of waves and rain drops, and a good family movie every now and then.</h4>
+             <h5>
+             <a href="mailto:angelinesyg@gmail.com">Let's connect to learn more about each other!</a>
+             </h5>
+
+             </div>
+           </div>
+        </div>
+    )
+}
+export default AboutContact
---
+++ src/pages/blogs.js
@@ -0,0 +1,13 @@
+import { divide } from 'lodash'
+import React from 'react' 
+
+const Blogs = () =>{
+    return (
+        <div>
+            <h1>Our Blogs</h1>
+            <h2>Coming Soon</h2>
+            
+        </div>
+    )
+}
+export default Blogs
