9018da22e85e31048e22d0a663b9a2b699023c33
AngelinesYG <79059491+AngelinesYG@users.noreply.github.com>
2021-08-31 13:22:57 -0400

    set up pages component and rendered blogs data

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,17 @@ module.exports = {
     description: 'Moms Blog was created by a mom for moms. With the idea that moms need a safe avenue to freely express themselves without being judged. By writing a blog about their experiences, moms can say whatever they need to say without fear of backlash and also find support from other moms experiencing or having experienced same or similar circumstances'
   },
 
-  plugins: ['gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-playground',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
+
 }
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,12 +1,39 @@
-import { divide } from 'lodash'
+// import { divide } from 'lodash'
 import React from 'react' 
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const Blogs = () =>{
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            Author
+                        }
+                        html
+                    }
+                }
+            }
+        }
+    `)
+    console.log(data)
     return (
         <Layout>
             <h1>Our Blogs</h1>
-            <h2>Coming Soon</h2>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge) =>{
+                    return (
+                        <li>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <h3>{edge.node.frontmatter.Author}</h3>
+                            <p>{edge.node.html}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
 
     )
